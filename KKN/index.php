afbf1f82c25e97f6ffbe38dc9085c98804574029
Puky-chan <rozi80.t@gmail.com>
2025-08-13 09:34:50 +0700

    Add files via upload

---
+++ KKN/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+    include 'navigasi/header.php'
+        ?>
+    <main>
+        <h2>Selamat datang di Website Edukasi P2L</h2>
+        <p>Website ini blah blah ......</p>
+    </main>
+</body>
+
+</html>
---
+++ KKN/navigasi/header.php
@@ -0,0 +1,15 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="img/logo-desa.jpg" alt="Logo Desa">
+            <h1>Pekarangan Pangan Lestari</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="profile.php">Edukasi P2L</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
